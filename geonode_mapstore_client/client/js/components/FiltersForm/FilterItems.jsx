4aaf2f5d56f1b13b72af3f847f7ecd398b15760f
Suren <dsuren1@gmail.com>
2023-07-28 13:39:21 +0530

    #1498 - Add image/icon property to filter option

--- geonode_mapstore_client/client/js/components/FiltersForm/FilterItems.jsx
+++ geonode_mapstore_client/client/js/components/FiltersForm/FilterItems.jsx
@@ -20,6 +20,7 @@ import withDebounceOnCallback from '@mapstore/framework/components/misc/enhancer
 import { getMessageById } from '@mapstore/framework/utils/LocaleUtils';
 import FilterByExtent from './FilterByExtent';
 import DateRangeFilter from './DateRangeFilter';
+import FaIcon from '../FaIcon';
 
 const FormControl = localizedProps('placeholder')(FormControlRB);
 function InputControl({ onChange, value, debounceTime, ...props }) {
@@ -29,10 +30,24 @@ const InputControlWithDebounce = withDebounceOnCallback('onChange', 'value')(Inp
 
 const SelectSync = localizedProps('placeholder')(ReactSelect);
 
+function Label({item} = {}, { messages }) {
+    return (
+        <div className="gn-label">
+            <span className="prefix">
+                {item.icon ? <FaIcon name={item.icon}/> : item.image ? <img src={item.image}/> : null}
+                {item.labelId ? getMessageById(messages, item.labelId) : <span>{item.label}</span>}
+            </span>
+            {!isNil(item.count) && <span className="facet-count">{`(${item.count})`}</span>}
+        </div>
+    );
+}
+Label.contextTypes = {
+    messages: PropTypes.object
+};
+
 function Facet({
     item,
     active,
-    label,
     onChange
 }) {
     const filterValue = item.filterValue || item.id;
@@ -46,8 +61,7 @@ function Facet({
                 onKeyDown={(event) => event.key === 'Enter' ? onChange() : null}
                 style={{ display: 'block', width: 0, height: 0, overflow: 'hidden', opacity: 0, padding: 0, margin: 0 }}
             />
-            {label}
-            {!isNil(item.count) && <span className="facet-count">{`(${item.count})`}</span>}
+            <Label item={item}/>
         </div>
     );
 }
@@ -236,7 +250,7 @@ function FilterItem({
         const renderFacet = ({item, active, onChangeFacet, renderChild}) => {
             return (
                 <div className="gn-facet-wrapper">
-                    <Facet label={item.labelId ? getMessageById(messages, item.labelId) : <span>{item.label}</span>} item={item} active={active} onChange={onChangeFacet}/>
+                    <Facet item={item} active={active} onChange={onChangeFacet}/>
                     {item.items && renderChild && <div className="facet-children">{renderChild()}</div>}
                 </div>
             );
@@ -262,8 +276,7 @@ function FilterItem({
                                 value={getFilterValue(item)}
                                 onChange={onChangeFilter}
                             >
-                                {item.labelId ? getMessageById(messages, item.labelId) : item.label}
-                                {!isNil(item.count) && <span className="facet-count">{`(${item.count})`}</span>}
+                                <Label item={item}/>
                             </Checkbox>
                         }
                     </div>
@@ -297,8 +310,7 @@ function FilterItem({
                         checked={!!active}
                         value={getFilterValue(field)}
                         onChange={onChangeFilterParent}>
-                        {field.labelId ? getMessageById(messages, field.labelId) : field.label}
-                        {!isNil(field.count) && <span className="facet-count">{`(${field.count})`}</span>}
+                        <Label item={field}/>
                         {filterChild()}
                     </Checkbox>
                 </FormGroup>
